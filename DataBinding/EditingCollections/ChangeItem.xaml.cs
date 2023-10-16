63c8192a42c2227c774ccc0ec9d3df11c0fcd958
guanyun <guanyun1024@icloud.com>
2023-10-16 20:05:07 +0800

    update

---
+++ DataBinding/EditingCollections/ChangeItem.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace EditingCollections;
+/// <summary>
+/// ChangeItem.xaml 的交互逻辑
+/// </summary>
+public partial class ChangeItem : Window {
+    public ChangeItem() {
+        InitializeComponent();
+    }
+
+    private void Submit_Click(object sender, RoutedEventArgs e) {
+        DialogResult = true;
+        Close();
+    }
+}
---
+++ DataBinding/EditingCollections/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+namespace EditingCollections;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window {
+    public MainWindow() {
+        InitializeComponent();
+    }
+
+    private void Add_Click(object sender, RoutedEventArgs e) {
+        var editableCollectionView = itemsControl.Items as IEditableCollectionView;
+        if (!editableCollectionView.CanAddNew) {
+            MessageBox.Show("You cannot add items to the list.");
+            return;
+        }
+
+        var win = new ChangeItem { DataContext = editableCollectionView.AddNew() };
+        if (win.ShowDialog().GetValueOrDefault()) {
+            editableCollectionView.CommitNew();
+        } else {
+            editableCollectionView.CancelNew();
+        }
+    }
+
+    private void Edit_Click(object sender, RoutedEventArgs e) {
+        if (itemsControl.SelectedItem == null) {
+            MessageBox.Show("No item is selected");
+            return;
+        }
+
+        var editableCollectionView = itemsControl.Items as IEditableCollectionView;
+        editableCollectionView.EditItem(itemsControl.SelectedItem);
+
+        var win = new ChangeItem();
+        win.DataContext = itemsControl.SelectedItem;
+        if (win.ShowDialog().GetValueOrDefault()) {
+            editableCollectionView.CommitEdit();
+        } else {
+            editableCollectionView.CancelEdit();
+        }
+
+    }
+
+    private void Remove_Click(object sender, RoutedEventArgs e) {
+        var item = itemsControl.SelectedItem as PurchaseItem;
+        if (item == null) {
+            MessageBox.Show("No item is selected");
+            return;
+        }
+
+        var editableCollectionView = itemsControl.Items as IEditableCollectionView;
+        if (!editableCollectionView.CanRemove) {
+            MessageBox.Show("You cannot remove items from the list.");
+            return;
+        }
+
+        if (MessageBox.Show("Are you sure you want to remove " + item.Description,
+               "Remove Item", MessageBoxButton.YesNo) == MessageBoxResult.Yes) {
+            editableCollectionView.Remove(itemsControl.SelectedItem);
+        }
+    }
+}
